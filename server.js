8128a8f78f027807654c2d2a58a74ae100c00af1
babinnepali <babinnepalibtl@gmail.com>
2025-04-14 19:02:45 +0545

    api url changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/v1/tasks', require('./routes/taskRoutes.js'));
+app.use('/tasks', require('./routes/taskRoutes.js'));
 
 
 
