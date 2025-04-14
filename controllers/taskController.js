1893e32ee05a5ef4f9d142320079a2a86c450427
babinnepali <babinnepalibtl@gmail.com>
2025-04-14 18:54:24 +0545

    successfully performed CRUD with nodejs and mySQL

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,5 @@
 const db = require('../config/db');
+
 const getTasks = async(req,res) => {
     try{
         const [data] = await db.query('SELECT * FROM tasks');
@@ -11,7 +12,8 @@ const getTasks = async(req,res) => {
         res.status(200).send({
             success: true,
             message: 'Tasks fetched successfully',
-            data
+            totalTasks: data.length,
+            data: data[0]
         })
     }catch(err){
         console.log(err);
@@ -24,6 +26,128 @@ const getTasks = async(req,res) => {
     }
 }
 
+const getTaskById = async(req,res)=>{
+    try{
+        const taskId = req.params.id
+        if(!taskId){
+            return res.status(404).send({
+                success: false,
+                message: 'Task ID is required'
+            })
+        }
+        const [data] = await db.query('SELECT * FROM tasks WHERE id = ?', [taskId]);
+        if(!data){
+            return res.status(404).send({
+                success: false,
+                message: 'No record found'
+            })
+        }
+        res.status(200).send({
+            success: true,
+            studentdetails: data[0]
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: 'Error fetching task',
+            error
+        })
+    }
+}
 
-module.exports = {getTasks}
+const createTask = async(req,res) =>{
+    try{
+        const{title,description} = req.body;
+        if(!title || !description){
+            return res.status(500).send({
+                success: false,
+                message: 'Title and description are required'
+            })
+        }
+        const [data] = await db.query('INSERT INTO tasks (title,description) VALUES (?,?)',[title,description]);
+        if(!data){
+            return res.status(500).send({
+                success: false,
+                message: 'Error creating task'
+            })
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Task created successfully',
+            data: data[0]
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error creating task',
+            error
+        })
+    }
+}
+
+const updateTask = async(req,res)=>{
+    try{
+        const taskId = req.params.id
+        if(!taskId){
+            return res.status(404).send({
+                success: false,
+                message: 'Task ID is required'
+            })
+        }
+        const {title,description} = req.body;
+        const [data] = await db.query('UPDATE tasks SET title = ?, description = ? WHERE id = ?', [title,description,taskId]);
+        if(!data){
+            return res.status(500).send({
+                success: false,
+                message: 'Error updating task'
+            })
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Task updated successfully',
+            data: data[0]
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: 'Error updating task',
+            error
+        }) 
+}
+}
+const deleteTask = async(req,res)=>{
+    try{
+        const taskId = req.params.id
+        if(!taskId){
+            return res.status(404).send({
+                success: false,
+                message: 'Task ID is required'
+            })
+        }
+        const [data] = await db.query('DELETE FROM tasks WHERE id = ?', [taskId]);
+        if(!data){
+            return res.status(500).send({
+                success: false,
+                message: 'Error deleting task'
+            })
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Task deleted successfully',
+            data: data.length
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: 'Error deleting task',
+            error
+        })
+    }
+}
+module.exports = {getTasks, getTaskById, createTask, updateTask,deleteTask}
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { getTasks } = require('../controllers/taskController');
+const { getTasks, getTaskById, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 
 
 const router = express.Router();
 
 
 router.get('/getall',getTasks)
+
+router.get('/get/:id',getTaskById)
+
+router.post('/create',createTask)
+
+router.put('/update/:id',updateTask)
+
+router.delete('/delete/:id', deleteTask)
 
 module.exports = router;
